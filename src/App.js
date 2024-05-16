528c81406d6208fd8b60517f159876ac2187a2ec
Rehman  Ali <seositesoft17@gmail.com>
2024-05-16 11:20:14 +0500

    updatecd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useCallback } from 'react';
+import Globe from 'react-globe.gl';
+import MapImage from './image/abc.jpg';
+const GlobeWithLabels = () => {
+  const globeRef = useRef(null);
 
-function App() {
+   // Data containing the labels
+   const labelsData = [
+    {
+      lat: 40.7128,
+      lng: -74.006,
+      label: "Pakistan"
+    }
+    // { labelLabel: 'New York',labelLat: 40.7128, labelLng: -74.006, labelText: "Abc" },
+    // { labelLabel: 'London', labelLat: 51.5074, labelLng: -0.1278, labelText: "abc" },
+    // Add more labels as needed
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100%', height: '500px' }}>
+      <Globe
+        ref={globeRef}
+        globeTextureUrl={MapImage}
+        backgroundColor="red"
+        globeImageUrl="https://unpkg.com/three-globe/example/img/earth-night.jpg"
+        backgroundImageUrl="https://unpkg.com/three-globe@2.31.0/example/img/night-sky.png"
+        labelsData={labelsData}
+        labelText={"label"}
+        labelSize={2.6}
+        labelColor={useCallback(() => "#a3ffa2", [])}
+        labelDotRadius={1}
+        labelAltitude={0.05}
+       
+     />
     </div>
   );
-}
+};
 
-export default App;
+export default GlobeWithLabels;
